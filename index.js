6cf3072841e9471e69172a7be453bac826119cec
Andrzej Rehmann <hoto@users.noreply.github.com>
2024-04-03 23:53:01 +0100

    Smol speshul chars

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-/**
- * https://github.com/sindresorhus/alfy
- */
 import alfy from 'alfy'
 
 const smolLetters = { 'a': 'ᵃ', 'b': 'ᵇ', 'c': 'ᶜ', 'd': 'ᵈ', 'e': 'ᵉ', 'f': 'ᶠ', 'g': 'ᵍ', 'h': 'ʰ', 'i': 'ⁱ', 'j': 'ʲ', 'k': 'ᵏ', 'l': 'ˡ', 'm': 'ᵐ', 'n': 'ⁿ', 'o': 'ᵒ', 'p': 'ᵖ', 'q': 'ᑫ', 'r': 'ʳ', 's': 'ˢ', 't': 'ᵗ', 'u': 'ᵘ', 'v': 'ᵛ', 'w': 'ʷ', 'x': 'ˣ', 'y': 'ʸ', 'z': 'ᶻ' }
+const smolNumbers = { '0': '⁰', '1': '¹', '2': '²', '3': '³', '4': '⁴', '5': '⁵', '6': '⁶', '7': '⁷', '8': '⁸', '9': '⁹' }
+const smolSpeshulChars = { '(': '⁽', ')': '⁾', '+': '⁺', '-': '⁻', '=': '⁼'}
 
-const smolize = (str) => str.split('').map(char => smolLetters[char] || char).join('')
+const smolize = (str) => str
+  .split('')
+  .map(char => smolLetters[char] || char)
+  .map(char => smolNumbers[char] || char)
+  .map(char => smolSpeshulChars[char] || char)
+  .join('')
 
-const items = [{
+alfy.output([{
   title: 'Smolerize',
   subtitle: smolize(alfy.input),
   arg: smolize(alfy.input)
-}]
-
-alfy.output(items)
+}])
